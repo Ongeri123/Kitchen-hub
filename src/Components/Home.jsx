c2a68cfce6ab1ce9f680f2b6bec158d3e0a3e513
Ongeri123 <ongerinewton10@gmail.com>
2025-07-31 21:32:54 +0300

    modified css and home file

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,20 @@
 function Home() {
     return (
         <div className="home">
-            <h1>Healthy & <br></br> Delicious <br></br> Fast Food</h1>
-            <p>"Feast Easy. We`ve Got You"</p>
+            <div className="heading1">
+                <h1>Healthy &</h1>
+                <h1 style={{padding:'0 120px'}}>Delicious</h1>
+                <h1 style={{padding: '0 30px'}}>Fast Food</h1>
+            </div>
+            <div className="variety-container">
+                <p>Explore various <br></br>options for you to pick</p>
+                <div className="variety">
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+            </div>
         </div>
     )
 }
