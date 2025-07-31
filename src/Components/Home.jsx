ec297807ae1be2c80805c5787c87185f8e4bbed5
Ongeri123 <ongerinewton10@gmail.com>
2025-07-31 19:37:01 +0300

    modified the files

---
+++ src/Components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="home">
+            <h1>Healthy & <br></br> Delicious <br></br> Fast Food</h1>
+            <p>"Feast Easy. We`ve Got You"</p>
+        </div>
+    )
+}
+
+export default Home;
